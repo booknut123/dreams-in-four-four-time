d0a4534f043f685f1959507280288aac697731c2
booknut123 <kailynmlau@gmail.com>
2025-07-21 13:52:52 -0700

    added lenis for smooth scrolling

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
-import { mount } from 'svelte'
-import './app.css'
+import { mount } from "svelte";
+import "./app.css";
 // @ts-ignore
-import App from './App.svelte'
+import App from "./App.svelte";
 
+// Using Lenis for smooth scroll
+import Lenis from "lenis";
+
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+// Mount Svelte app
 const app = mount(App, {
-  target: document.getElementById('app'),
-})
+  target: document.getElementById("app"),
+});
 
-export default app
+export default app;
